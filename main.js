ccfa99124b2320e64f5c4f719d3ac71146229dec
Caitlin <caitlin.smith007@gmail.com>
2015-06-02 20:29:03 -0700

    message received, over and out

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 alert("I'm working with JavaScript");
+
+function mad_libs() {
+	document.write ("There once was a man from Nantucket, Who kept all his cash in a bucket.  But his daughter, named Nan, Ran away with a man And as for the bucket, Nantucket.");
+}
+
+mad_libs();
